306cf0a4c055acd3013d48c96da7910a99e0d63b
Champ <kuldeep3727@gmail.com>
2024-10-19 13:16:16 +0530

    Day-5 additional problem#3

--- src/com/scaler/month_1/Day5_AdditionalProbalems.java
+++ src/com/scaler/month_1/Day5_AdditionalProbalems.java
@@ -4,19 +4,41 @@ import java.util.Scanner;
 
 class Day5_AdditionalProbalems {
     public static void main(String[] args) {
-        //Q#2
+        //Q#3
         Scanner sc = new Scanner(System.in);
-        Integer costPrice = sc.nextInt();
-        Integer sellPrice = sc.nextInt();
-
-        if(costPrice<sellPrice){
-            System.out.println("1");
-        }else {
-            System.out.println("-1");
+        int rating = sc.nextInt();
+        String category = null;
+        
+        if(rating>=2100){
+            category = "grand master";
+        } else if (rating>=1900) {
+            category = "candidate master";
+        } else if (rating>1600) {
+            category = "expert";
+        } else if (rating>=1400) {
+            category = "pupil";
+        } else if (rating<=1400) {
+            category = "newbie";
         }
 
-        Integer profit=0;
-        profit = Math.abs(sellPrice-costPrice);
-        System.out.println(profit);
+        if(rating%2==0){
+            category = category.toUpperCase();
+        }
+        System.out.println(category);
+
+        //Q#2
+//        Scanner sc = new Scanner(System.in);
+//        Integer costPrice = sc.nextInt();
+//        Integer sellPrice = sc.nextInt();
+//
+//        if(costPrice<sellPrice){
+//            System.out.println("1");
+//        }else {
+//            System.out.println("-1");
+//        }
+//
+//        Integer profit=0;
+//        profit = Math.abs(sellPrice-costPrice);
+//        System.out.println(profit);
     }
 }
