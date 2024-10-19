1d426c7828c73cf19224b8a962b69e252f94d826
Champ <kuldeep3727@gmail.com>
2024-10-19 13:27:23 +0530

    Day-5 additional problem#4

--- src/com/scaler/month_1/Day5_AdditionalProbalems.java
+++ src/com/scaler/month_1/Day5_AdditionalProbalems.java
@@ -4,27 +4,60 @@ import java.util.Scanner;
 
 class Day5_AdditionalProbalems {
     public static void main(String[] args) {
-        //Q#3
+        //Q#4
         Scanner sc = new Scanner(System.in);
-        int rating = sc.nextInt();
-        String category = null;
-        
-        if(rating>=2100){
-            category = "grand master";
-        } else if (rating>=1900) {
-            category = "candidate master";
-        } else if (rating>1600) {
-            category = "expert";
-        } else if (rating>=1400) {
-            category = "pupil";
-        } else if (rating<=1400) {
-            category = "newbie";
+        int x = sc.nextInt();
+        sc.close();
+
+        switch (x){
+            case 1:
+                System.out.println(31);
+            case 2:
+                System.out.println(28);
+            case 3:
+                System.out.println(31);
+            case 4:
+                System.out.println(30);
+            case 5:
+                System.out.println(31);
+            case 6:
+                System.out.println(30);
+            case 7:
+                System.out.println(31);
+            case 8:
+                System.out.println(31);
+            case 9:
+                System.out.println(30);
+            case 10:
+                System.out.println(31);
+            case 11:
+                System.out.println(30);
+            case 12:
+                System.out.println(31);
         }
 
-        if(rating%2==0){
-            category = category.toUpperCase();
-        }
-        System.out.println(category);
+
+//        //Q#3
+//        Scanner sc = new Scanner(System.in);
+//        int rating = sc.nextInt();
+//        String category = null;
+//
+//        if(rating>=2100){
+//            category = "grand master";
+//        } else if (rating>=1900) {
+//            category = "candidate master";
+//        } else if (rating>1600) {
+//            category = "expert";
+//        } else if (rating>=1400) {
+//            category = "pupil";
+//        } else if (rating<=1400) {
+//            category = "newbie";
+//        }
+//
+//        if(rating%2==0){
+//            category = category.toUpperCase();
+//        }
+//        System.out.println(category);
 
         //Q#2
 //        Scanner sc = new Scanner(System.in);
