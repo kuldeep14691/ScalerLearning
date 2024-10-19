957ed8c16c6f700c5c1ddabde339f047f484e21a
Champ <kuldeep3727@gmail.com>
2024-10-19 18:49:59 +0530

    Day-6 Additional#3

--- src/com/scaler/month_1/Day6_AdditionalProblems.java
+++ src/com/scaler/month_1/Day6_AdditionalProblems.java
@@ -4,17 +4,30 @@ import java.util.Scanner;
 
 public class Day6_AdditionalProblems {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int count=1;
-        int evenSum=0;
+//        Scanner sc = new Scanner(System.in);
+//        int x = sc.nextInt();
+//
+//        int count=1;
+//        int evenSum=0;
+//
+//        while (count<=x){
+//            if (count%2 == 0){
+//                evenSum += count;
+//            }
+//            count++;
+//        }
+//        System.out.println(evenSum);
 
-        while (count<=x){
-            if (count%2 == 0){
-                evenSum += count;
-            }
-            count++;
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result=1;
+
+        while(b>0){
+            result *= a;
+            b--;
         }
-        System.out.println(evenSum);
+//        result = (int) Math.pow(a,b);
+        System.out.println(result);
     }
 }
