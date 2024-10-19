f26e1e1d45982a2e12d435fd4fcfbccb81380e01
Champ <kuldeep3727@gmail.com>
2024-10-19 20:08:53 +0530

    Day-6 Additional#6

--- src/com/scaler/month_1/Day6_AdditionalProblems.java
+++ src/com/scaler/month_1/Day6_AdditionalProblems.java
@@ -41,35 +41,53 @@ public class Day6_AdditionalProblems {
 //            count++;
 //        }
 
-        long balance;
-        long numberOfOperations;
-        long count;
+//        long balance;
+//        long numberOfOperations;
+//        long count;
+//
+//        Scanner sc = new Scanner(System.in);
+//        balance = sc.nextLong();
+//        numberOfOperations = sc.nextLong();
+//
+//        long[] operations_type = new long[(int) numberOfOperations];    //1=credit 2=debit
+//        long[] operations_amount = new long[(int) numberOfOperations];
+//
+//
+//        for (count=0;count<numberOfOperations;count++){
+//            operations_type[(int) count] = sc.nextLong();
+//            operations_amount[(int) count] = sc.nextLong();
+//
+//            if (operations_type[(int) count] == 1){
+//                balance += operations_amount[(int) count];
+//                System.out.println(balance);
+//            } else if (operations_type[(int) count]==2) {
+//                if (operations_amount[(int) count]>balance){
+//                    System.out.println("Insufficient Funds");
+//                }else {
+//                    balance -= operations_amount[(int) count];
+//                    System.out.println(balance);
+//                }
+//            }
+//        }
+//
+//        sc.close();
+
 
         Scanner sc = new Scanner(System.in);
-        balance = sc.nextLong();
-        numberOfOperations = sc.nextLong();
-
-        long[] operations_type = new long[(int) numberOfOperations];    //1=credit 2=debit
-        long[] operations_amount = new long[(int) numberOfOperations];
-
-
-        for (count=0;count<numberOfOperations;count++){
-            operations_type[(int) count] = sc.nextLong();
-            operations_amount[(int) count] = sc.nextLong();
-
-            if (operations_type[(int) count] == 1){
-                balance += operations_amount[(int) count];
-                System.out.println(balance);
-            } else if (operations_type[(int) count]==2) {
-                if (operations_amount[(int) count]>balance){
-                    System.out.println("Insufficient Funds");
-                }else {
-                    balance -= operations_amount[(int) count];
-                    System.out.println(balance);
-                }
-            }
+        int x = sc.nextInt();
+        int number;
+        int reverseNumber = 0;
+        number = x;
+        while (number>0){
+            reverseNumber = reverseNumber*10 + number%10;
+            number = number/10;
         }
 
-        sc.close();
+        if (x==reverseNumber){
+            System.out.println("Yes");
+        }else {
+            System.out.println("No");
+        }
+
     }
 }
