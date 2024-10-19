6d6c33d418a940f2499ea14862e08ef6567cd395
Champ <kuldeep3727@gmail.com>
2024-10-19 15:10:09 +0530

    Day-5 additional problem#7

--- src/com/scaler/month_1/BankAccount.java
+++ src/com/scaler/month_1/BankAccount.java
@@ -5,21 +5,27 @@ import java.util.Scanner;
 public class BankAccount {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int balance = sc.nextInt();
-        int type = sc.nextInt();
-        int amount = sc.nextInt();
-
-        if(type==1){
-            balance += amount;
-        } else if (type==2) {
-            if (amount>balance){
-                System.out.println("Insufficient Funds");
-            }
-            balance -= amount;
-        } else {
-            System.out.println("Invalid operation");
+//        int balance = sc.nextInt();
+//        int type = sc.nextInt();
+//        int amount = sc.nextInt();
+//
+//        if(type==1){
+//            balance += amount;
+//        } else if (type==2) {
+//            if (amount>balance){
+//                System.out.println("Insufficient Funds");
+//            }
+//            balance -= amount;
+//        } else {
+//            System.out.println("Invalid operation");
+//        }
+//        System.out.println(balance);
+        int num = sc.nextInt();
+        if(num%5==0 && num%11==0){
+            System.out.println(1);
+        }else {
+            System.out.println(0);
         }
-        System.out.println(balance);
 
     }
 }
