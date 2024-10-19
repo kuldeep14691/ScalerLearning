b0462f6cbe511795e5bb1e71daead6f0652445a0
Champ <kuldeep3727@gmail.com>
2024-10-19 06:18:49 +0530

    Day-5 refresher assignments

---
+++ src/com/scaler/month_1/Main.java
@@ -0,0 +1,89 @@
+package com.scaler.month_1;
+
+import java.lang.invoke.SwitchPoint;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Scanner sc = new Scanner(System.in);
+//        int angle1 = sc.nextInt();
+//        int angle2 = sc.nextInt();
+//        int angle3 = sc.nextInt();
+//
+//        if(angle1+angle2+angle3 = 180){
+//            System.out.print("1");
+//        } else {
+//            System.out.print("0");
+//        }
+
+
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        if(n%3==0 && n%5==0){
+//            System.out.println("FizzBuzz");
+//        } else if (n%5==0){
+//            System.out.println("Buzz");
+//        } else if (n%3==0){
+//            System.out.println("Fizz");
+//        }
+
+
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int lastDigit = n%10;
+//        System.out.println(lastDigit);
+
+
+//        Scanner sc = new Scanner(System.in);
+//        int num1 = sc.nextInt();
+//        int remainingUnits=0;
+//        float totalBill=0;
+//
+//        remainingUnits = num1;
+//
+//        if(remainingUnits >= 50){
+//            totalBill = (float) (50*0.50);
+//            remainingUnits = remainingUnits-50;
+//        } else {
+//            totalBill = (float) (remainingUnits*0.50);
+//            remainingUnits = 0;
+//        }
+//
+//        if(remainingUnits>0) {
+//            if (remainingUnits <= 100) {
+//                totalBill = totalBill + (float) (remainingUnits * 0.75);
+//                remainingUnits = 0;
+//            } else {
+//                totalBill = totalBill + (float) (100 * 0.75);
+//                remainingUnits = remainingUnits - 100;
+//            }
+//        }
+//
+//        if(remainingUnits>0) {
+//            if (remainingUnits <= 100) {
+//                totalBill = (float) (totalBill + (remainingUnits * 1.20));
+//                remainingUnits = 0;
+//            } else {
+//                totalBill = totalBill + (float) (100 * 1.2);
+//                remainingUnits = remainingUnits - 100;
+//            }
+//        }
+//
+//        if(remainingUnits>0) {
+//                totalBill = totalBill + (float) (remainingUnits * 1.5);
+//        }
+//
+//
+//        //add surcharge
+//        totalBill = totalBill+ (totalBill*20/100);
+//        System.out.println((int) totalBill);
+
+    }
+
+
+}
